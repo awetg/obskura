fece096c51d8adc27402c95a75ac640374b128df
Phyllis Lai <phyllislai@Phylliss-MacBook-Pro.local>
2017-11-30 18:20:55 +0200

    Elsa's scene revised

--- Obskura/Assets/Scripts/EnemyAI.cs
+++ Obskura/Assets/Scripts/EnemyAI.cs
@@ -4,29 +4,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAi : MonoBehaviour 
-{
-	//public Transform[] patrolPoints;
-	public float speed;
-	//Transform currentPatrolPoint;
-	//int currentPatrolIndex;
-
+public class EnemyAI : MonoBehaviour 
+{	public float speed;
 	public Transform target;
 	public float chaseRange;
 
-	void Start ()
-	{
-		//currentPatrolIndex =0;
-		//currentPatrolPoint = patrolPoints [currentPatrolIndex];
-	}
+
 	void Update ()
 	{
 		float distanceToTarget =Vector3.Distance(transform.position, target.position);
 		if(distanceToTarget <chaseRange)
 		{
 			Vector3 targetDir = target.position - transform.position;
+			//find out where the player is
 			float angle=Mathf.Atan2 (targetDir.y,targetDir.x)*Mathf.Rad2Deg-90f;
 			Quaternion q=Quaternion.AngleAxis (angle,Vector3.forward);
+			//turns towards the player
 			transform.rotation = Quaternion.RotateTowards (transform.rotation, q, 180);
 			transform.Translate (Vector3.up*Time.deltaTime*speed);
 		}
