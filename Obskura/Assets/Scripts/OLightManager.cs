325337db9e234857166d3d290a4f75cce9fff3b9
Manuel Furia <manuelfuria94@gmail.com>
2017-11-30 22:20:40 +0200

    Added support for UICamera

--- Obskura/Assets/Scripts/OLightManager.cs
+++ Obskura/Assets/Scripts/OLightManager.cs
@@ -7,10 +7,12 @@ public class OLightManager : MonoBehaviour {
 
 	public Camera LightCamera;
 	public Camera MainCamera;
+	public Camera UICamera;
 	public float Intensity = 10.0F;
 	public Color Overlay = new Color (0.0F, 0.0F, 0.0F);
 	private Material material;
 	private RenderTexture lightMap;
+	private RenderTexture uiTexture;
 
 	// Creates a private material used to the effect
 	void Awake ()
@@ -21,6 +23,11 @@ public class OLightManager : MonoBehaviour {
 		LightCamera.orthographicSize = MainCamera.orthographicSize;
 		LightCamera.aspect = MainCamera.aspect;
 
+		UICamera.targetTexture = new RenderTexture(LightCamera.pixelWidth, LightCamera.pixelHeight, 24);
+
+		UICamera.orthographicSize = MainCamera.orthographicSize;
+		UICamera.aspect = MainCamera.aspect;
+
 		RefreshVertices ();
 	}
 
@@ -29,15 +36,21 @@ public class OLightManager : MonoBehaviour {
 			LightCamera.orthographicSize = MainCamera.orthographicSize;
 			LightCamera.aspect = MainCamera.aspect;
 		}
+		if (UICamera.aspect != MainCamera.aspect || UICamera.orthographicSize != MainCamera.orthographicSize) {
+			UICamera.orthographicSize = MainCamera.orthographicSize;
+			UICamera.aspect = MainCamera.aspect;
+		}
 	}
 
 	// Postprocess the image
 	void OnRenderImage (RenderTexture source, RenderTexture destination)
 	{
 		lightMap = LightCamera.targetTexture;
+		uiTexture = UICamera.targetTexture;
 		material.SetFloat("_Intensity", Intensity);
 		material.SetColor("_Overlay", Overlay);
 		material.SetTexture ("_LightTex", lightMap);
+		material.SetTexture ("_UITex", uiTexture);
 		Graphics.Blit (source, destination, material);
 	}
 
